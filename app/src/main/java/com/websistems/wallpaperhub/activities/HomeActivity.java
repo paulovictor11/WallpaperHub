169dc5ab45cac9eefe5c6e1a3d43e596091a9c36
paulovictor11 <pauloalmeidajp652@gmail.com>
2018-08-12 18:41:24 -0300

    Versão 0.4

--- app/src/main/java/com/websistems/wallpaperhub/activities/HomeActivity.java
+++ app/src/main/java/com/websistems/wallpaperhub/activities/HomeActivity.java
@@ -1,15 +1,60 @@
 package com.websistems.wallpaperhub.activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.websistems.wallpaperhub.R;
+import com.websistems.wallpaperhub.fragments.HomeFragment;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        displayFragment(new HomeFragment());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment;
+
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.nav_fav:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.nav_set:
+                fragment = new HomeFragment();
+                break;
+
+            default:
+                fragment = new HomeFragment();
+        }
+
+        displayFragment(fragment);
+
+        return true;
+    }
+
+    private void displayFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_area, fragment)
+                .commit();
     }
 }
